2cf9ac779395325633bb1d9f750cec7596a2acdb
alihaydermahdi <alihaydermahdi@gmail.com>
2026-04-06 01:09:02 +0100

    Update doctorsData.js

--- doctorsData.js
+++ doctorsData.js
@@ -1,32 +1,18 @@
-// قاعدة بيانات أطباء كشفيّة
+// The Master Database for Kashfeya Doctors
 const doctorsData = [
     {
         id: 1,
         name: "د. أحمد محمود",
-        specialty: "internal", // باطنية
+        specialty: "internal",
         specialtyAr: "أخصائي باطنية وقلبية",
-        province: "baghdad", // المحافظة
+        province: "baghdad",
         provinceAr: "بغداد",
-        address: "الحارثية - شارع الكندي - مجمع العيادات",
-        lat: 33.3128, // الإحداثيات لخرائط جوجل
+        address: "الحارثية - شارع الكندي",
+        lat: 33.3128,
         lng: 44.3615,
         phone: "07700000000",
         hours: "4:00 م - 8:00 م",
         img: "https://ui-avatars.com/api/?name=د+احمد&background=005eb8&color=fff"
     },
-    {
-        id: 2,
-        name: "د. سارة علي",
-        specialty: "gynae", // نسائية
-        specialtyAr: "أخصائية نسائية وتوليد",
-        province: "erbil",
-        provinceAr: "أربيل",
-        address: "شارع 40 - مقابل مستشفى رزگاري",
-        lat: 36.1901,
-        lng: 44.0091,
-        phone: "07500000000",
-        hours: "3:00 م - 7:00 م",
-        img: "https://ui-avatars.com/api/?name=د+سارة&background=005eb8&color=fff"
-    }
-    // تگدر تضيف أي عدد من الأطباء هنا بنفس الطريقة
+    // Add more doctors here following the same structure
 ];
